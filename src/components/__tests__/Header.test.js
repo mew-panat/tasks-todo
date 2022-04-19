08b3ea8be4545f0a205e4bbb23ae9076dce61800
mew <mewchildy@gmail.com>
2022-04-19 21:50:49 +0700

    fix failed test

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -4,7 +4,7 @@ import Header from '../Header';
 describe('header', () => {
   it('should show name of header', () => {
     render(<Header />);
-    const nameEl = screen.getByText(/tewt/);
+    const nameEl = screen.getByText(/todo list/);
     expect(nameEl).toBeInTheDocument();
   });
 });
