3ba76d631241848c9126567e4f1cbf758ca500e3
mew <mewchildy@gmail.com>
2022-04-27 18:03:20 +0700

    add test to side bar

--- src/components/__tests__/SideBar.test.js
+++ src/components/__tests__/SideBar.test.js
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import SideBar from '../SideBar'
 
 describe('sidebar', () => {
-  it('should check sidebar', () => {
+  it('should check sidebar today', () => {
     render(<SideBar />)
     const nameEl = screen.getByText(/Today/);
+    expect(nameEl).toBeInTheDocument();
+  })
+
+  it('should check sidebar Inbox', () => {
+    render(<SideBar />)
+    const nameEl = screen.getByText(/Inbox/);
     expect(nameEl).toBeInTheDocument();
   })
 });
